47e149aed3166d31d868cfb510da431ae3ad4028
Giovanni Barbiero <38290406+GioBar00@users.noreply.github.com>
2023-11-05 16:51:49 +0100

    fixed data cleansing

--- dataset_cleansing.py
+++ dataset_cleansing.py
@@ -1,18 +1,28 @@
 import numpy as np
 
-DATASET = np.load("./res/npz/public_data.npz", allow_pickle=True)
+DATASET = np.load("public_data.npz", allow_pickle=True)
 KEYS = list(DATASET.keys())
 IMAGES = DATASET[KEYS[0]]
 LABELS = DATASET[KEYS[1]]
 
-indices_meme = []
+#indices_meme = []
 
 shrek = IMAGES[506]
 trololo = IMAGES[529]
+
+# remove duplicates
+dup_indexes = np.unique(IMAGES, axis=0, return_index=True)[1]
+IMAGES = [IMAGES[index] for index in sorted(dup_indexes)]
+LABELS = [LABELS[index] for index in sorted(dup_indexes)]
+
+# remove shrek and trololo
+meme_indexes = []
 for i, image in enumerate(IMAGES):
     if  np.array_equal(IMAGES[i], shrek) or np.array_equal(IMAGES[i], trololo):
-        indices_meme.append(i)
+        meme_indexes.append(i)
+IMAGES = np.delete(IMAGES, meme_indexes, axis=0)
+LABELS = np.delete(LABELS, meme_indexes, axis=0)
 
-IMAGES = np.delete(IMAGES, indices_meme, axis=0)
+print(len(IMAGES))
 
-np.savez("./res/npz/public_data_clean.npz", IMAGES, LABELS)
+np.savez("public_data_clean.npz", IMAGES, LABELS)
