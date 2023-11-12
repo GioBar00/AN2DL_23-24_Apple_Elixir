330befcd2c1d957ff56f3be029e3baac585fc896
Giovanni Barbiero <38290406+GioBar00@users.noreply.github.com>
2023-11-12 04:03:44 +0100

    uploaded new notebooks

--- res/scripts/utils/dataset_balance_gio.py
+++ res/scripts/utils/dataset_balance_gio.py
@@ -19,7 +19,8 @@ def plot_images(images, labels, index=0, rows=1, cols=1, normalized=False):
 def random_90_rotation_flip(images, labels, num_gen):
     gen_images_shape = (num_gen, images.shape[1], images.shape[2], images.shape[3])
     gen_images = np.zeros(gen_images_shape)
-    gen_labels = np.zeros(num_gen)
+    # array of labels (strings) for generated images
+    gen_labels = np.empty(num_gen, dtype=object)
     ops = []
     for j in range(1, 4):
         ops.append(lambda x: np.rot90(x, j + 1))
@@ -37,20 +38,16 @@ def random_90_rotation_flip(images, labels, num_gen):
     return gen_images, gen_labels
 
 
-dataset = np.load('public_data_clean.npz', allow_pickle=True)
-keys = list(dataset.keys())
-images = np.array(dataset[keys[0]])
-labels = np.array(dataset[keys[1]])
-
-labels_map = {0: "healthy", 1: "unhealthy"}
-labels_rev_map = {"healthy": 0, "unhealthy": 1}
-labels = np.array([labels_rev_map[label] for label in labels])
+images, labels = np.load('public_data_clean.npz', allow_pickle=True).values()
 
 print('Images: ', len(images))
 print('Labels: ', len(labels))
 
-healthy_images = images[labels == 0]
-unhealthy_images = images[labels == 1]
+print('Images shape: ', images.shape)
+print('Labels shape: ', labels.shape)
+
+healthy_images = images[labels == 'healthy']
+unhealthy_images = images[labels == 'unhealthy']
 
 print('Healthy images: ', len(healthy_images))
 print('Unhealthy images: ', len(unhealthy_images))
@@ -59,28 +56,39 @@ print('Difference: ', diff)
 
 if abs(diff) > 0:
     if len(healthy_images) < len(unhealthy_images):
-        gen_images, gen_labels = healthy_images, labels[labels == 0]
+        gen_images, gen_labels = healthy_images, labels[labels == 'healthy']
     else:
-        gen_images, gen_labels = unhealthy_images, labels[labels == 1]
+        gen_images, gen_labels = unhealthy_images, labels[labels == 'unhealthy']
 
     gen_images, gen_labels = random_90_rotation_flip(gen_images, gen_labels, abs(diff))
-    plot_images(gen_images, gen_labels, rows=3, cols=3, normalized=False)
+    # plot_images(gen_images, gen_labels, rows=3, cols=3, normalized=False)
+    print('Generated images: ', len(gen_images))
+    print('Generated labels: ', len(gen_labels))
     images = np.concatenate((images, gen_images))
     labels = np.concatenate((labels, gen_labels))
 
     print('Images: ', len(images))
     print('Labels: ', len(labels))
-    print('Healthy images: ', len(images[labels == 0]))
-    print('Unhealthy images: ', len(images[labels == 1]))
+    print('Healthy images: ', len(images[labels == 'healthy']))
+    print('Unhealthy images: ', len(images[labels == 'unhealthy']))
 
 # shuffle images and labels in the same way
-idxs = np.arange(len(images))
-np.random.shuffle(idxs)
-images = images[idxs]
-labels = labels[idxs]
+# idxs = np.arange(len(images))
+# np.random.shuffle(idxs)
+# images = images[idxs]
+# labels = labels[idxs]
 
 # split dataset into train-validation and test sets
-X_tr_val, X_test, y_tr_val, y_test = train_test_split(images, labels, test_size=0.15, stratify=labels)
+#X_tr_val, X_test, y_tr_val, y_test = train_test_split(images, labels, test_size=0.15, stratify=labels)
+
+# save dataset as dictionary
+
+dataset = {
+    'images': images,
+    'labels': labels
+}
+
+np.savez_compressed('public_data_clean_balanced.npz', **dataset)
 
 # save train-validation and test sets
 #np.savez_compressed('public_data_clean_balanced_splitted.npz', X_tr_val, y_tr_val, X_test, y_test)
